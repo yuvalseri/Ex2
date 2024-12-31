4244a7511bfa9667ff6b400f74cb35ce5f66aa2d
yuval <yuvalseri10@gmail.com>
2024-12-31 23:06:40 +0200

    ניסיון

--- src/ex2/CellEntry.java
+++ src/ex2/CellEntry.java
@@ -2,11 +2,18 @@ package ex2;
 // Add your documentation below:
 
 public class CellEntry  implements Index2D {
-
+private String XY;
 
     @Override
     public boolean isValid() {
-        return false;
+        char firstchar= XY.charAt(0);
+        String numPart =XY.substring(1);
+        int number = Integer.parseInt(numPart);
+
+        if(!Character.isLetter(firstchar) && (0>number || number>99 )){
+            return false;
+        }
+        return true;
     }
 
     @Override
